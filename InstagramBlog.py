584d9b78e220ddd2cad00334a988fee825e77166
Danny Sivan <danny.sivan@gmail.com>
2017-03-10 13:45:12 +0200

    TumblrSource , queue function

--- InstagramBlog.py
+++ InstagramBlog.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod
 
-
+#TODO implement unimplemented methods
 class InstagramPic:
     def __init__(self,path,likes,description,tags):
         self.path = path
--- TumblrBlog.py
+++ TumblrBlog.py
@@ -1,12 +1,29 @@
-
+import pytumblr
+#TODO implement unimplemented methods
+#TODO test dump/reload mechanics
 
 class TumblrBlog:
-    def __init__(self,name,client=None):
+    def __init__(self,name,source,client=None):
         self.client = client
         self.name = name
 
     def queue(self,picture,**kwargs):
-        pass
+        #TODO think of a more pretty design so we can remove this if
+        if kwargs["picSource"] == "link":
+            response = self.client.create_photo(self.name,
+                                           state="queue",
+                                           tags=kwargs["tags"],
+                                           format="markdown",
+                                           source = picture,
+                                           caption=kwargs["caption"])
+        else: response = self.client.create_photo(self.name,
+                                           state="queue",
+                                           tags=kwargs["tags"],
+                                           format="markdown",
+                                           data = picture,
+                                           caption=kwargs["caption"])
+        print(response)
+
     def getQueueSize(self):
         pass
     def post(self,picture,**kwargs):
@@ -17,6 +34,17 @@ class TumblrBlog:
         pass
 
 
+class TumblrSource:
+    sourceTypeEnum = ["dir","set-file"]
+    def __init__(self,sourceType,path):
+        if sourceType not in self.descEnum:
+            raise "wrong desc"
+        #TODO check if path exists and is set file or dicectory
+        self.sourceType = sourceType
+        self.path = path
+
+    def getPics(self,amount):
+        pass
 
 
 
