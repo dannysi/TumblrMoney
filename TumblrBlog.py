35a079a6476407ad7e641641c0f9a52f55a7a382
Danny Sivan <danny.sivan@gmail.com>
2017-03-14 21:28:57 +0200

    TumblrBlog continuation
    Tumblr posting from source enum were added
    picture source enum were added
    TumblrPhoto was copied from prev project
    implemented prettier design for the queue method

--- TumblrBlog.py
+++ TumblrBlog.py
@@ -1,27 +1,49 @@
+import datetime
 import pytumblr
+from enum import Enum
 #TODO implement unimplemented methods
 #TODO test dump/reload mechanics
+
+
+
+class PictureSourceType(Enum):
+    LINK = 1
+    FILE = 2
+
+class TumblrSourceType(Enum):
+    DIR = 1
+    SET_FILE = 2
+
+
+class TumblrPhoto:
+    def __init__(self, post):
+        self.photos = [post["photos"][i]["original_size"]["url"] for i in post["photos"] ]
+        self.notes = post["note_count"]
+        self.reblog = post["reblogged_root_name"] if "reblogged_root_name" in post else None
+        #TODO parse tags
+        self.tags = []
+        self.date = datetime.datetime.fromtimestamp(post["timestamp"])
+        self.id = post["id"]
+        self.reblog_key = post["reblog_key"]
+
+
+
+
 
 class TumblrBlog:
     def __init__(self,name,source,client=None):
         self.client = client
         self.name = name
+        self.source = source
 
     def queue(self,picture,**kwargs):
-        #TODO think of a more pretty design so we can remove this if
-        if kwargs["picSource"] == "link":
-            response = self.client.create_photo(self.name,
-                                           state="queue",
-                                           tags=kwargs["tags"],
-                                           format="markdown",
-                                           source = picture,
-                                           caption=kwargs["caption"])
-        else: response = self.client.create_photo(self.name,
-                                           state="queue",
-                                           tags=kwargs["tags"],
-                                           format="markdown",
-                                           data = picture,
-                                           caption=kwargs["caption"])
+        queueDict = dict()
+        queueDict["state"] = "queue"
+        queueDict["tags"] = kwargs["tags"]
+        queueDict["format"] = "markdown"
+        queueDict["source" if kwargs["picSource"] == PictureSourceType.LINK else "data"] = picture
+        queueDict["caption"] = kwargs["caption"]
+        response = self.client.create_photo(self.name,**queueDict)
         print(response)
 
     def getQueueSize(self):
@@ -35,18 +57,13 @@ class TumblrBlog:
 
 
 class TumblrSource:
-    sourceTypeEnum = ["dir","set-file"]
     def __init__(self,sourceType,path):
-        if sourceType not in self.descEnum:
-            raise "wrong desc"
+        if type(sourceType) != TumblrSourceType:
+            raise "wrong source type"
         #TODO check if path exists and is set file or dicectory
         self.sourceType = sourceType
         self.path = path
 
     def getPics(self,amount):
         pass
-
-
-
-
 
